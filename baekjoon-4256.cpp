39698f7d267a1a09218f6d03c3b942cf003af414
2020056480 <fastdodge7@hanyang.ac.kr>
2023-09-15 10:55:44 +0900

    백준 4256 트리

---
+++ baekjoon-4256.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int T;
+int inorder[1000];
+int preorder[1000];
+
+void solve(int inStart, int inEnd, int preStart, int preEnd)
+{
+	if(inStart >= inEnd) return;
+	if(inStart + 1 == inEnd)
+	{
+		cout << inorder[inStart] << ' ';
+		return;
+	}
+	int leftCnt = 0;
+	for(int i = inStart; i < inEnd; i++)
+	{
+		
+		if(preorder[preStart] == inorder[i])
+			break;
+		leftCnt++;
+	}
+
+	solve(inStart, inStart + leftCnt, preStart + 1, preStart + leftCnt + 1);
+	solve(inStart + leftCnt + 1, inEnd, preStart + leftCnt + 1, preEnd);
+	cout << preorder[preStart] << ' ';
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> T;
+	for(int test_case = 1; test_case <= T; test_case++)
+	{
+		int N; cin >> N;
+		for(int i = 0; i < N; i++)
+			cin >> preorder[i];
+		
+		for(int i = 0; i < N; i++)
+			cin >> inorder[i];
+
+		solve(0, N, 0, N);
+		cout << '\n';
+		fill(preorder, preorder + 1000, 0);
+		fill(inorder, inorder + 1000, 0);
+	}
+
+	return 0;
+}
