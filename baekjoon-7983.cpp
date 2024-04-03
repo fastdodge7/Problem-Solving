2eab86af2e2207d55e2d58e278b9cca2a521b322
fastdodge7 <fastdodge7@gmail.com>
2024-04-03 13:50:39 +0900

    백준 7983 - 내일 할거야 : 간단한 정렬 그리디.

---
+++ baekjoon-7983.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int N;
+vector<pair<int, int>> arr;
+
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N;
+    for(int i = 0; i < N; i++)
+    {
+        int a, b; cin >> a >> b;
+        arr.push_back({b, a});
+    }
+
+    sort(arr.begin(), arr.end(), greater<pair<int, int>>());
+
+    int lastTimeStamp = arr[0].first;
+    for(int i = 0; i < N; i++)
+    {
+        if(lastTimeStamp > arr[i].first)
+            lastTimeStamp = arr[i].first;
+        lastTimeStamp = lastTimeStamp - arr[i].second;
+    }
+
+    cout << lastTimeStamp;
+    return 0;
+}
